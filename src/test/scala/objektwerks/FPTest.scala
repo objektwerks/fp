733386f5a5f6648a6bfca233c5c1dd695b94a652
objektwerks <objektwerks@runbox.com>
2025-04-10 12:07:19 -0400

    added fp test

---
+++ src/test/scala/objektwerks/FPTest.scala
@@ -0,0 +1,8 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+final class FPTest extends AnyFunSuite with Matchers:
+  test("todo"):
+    1 shouldBe 1
