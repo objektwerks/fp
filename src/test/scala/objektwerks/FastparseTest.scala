be7946a593ac7f87585a47033e20aba8a92572d1
objektwerks <objektwerks@runbox.com>
2025-04-14 18:09:40 -0400

    renamed test

--- src/test/scala/objektwerks/FastparseTest.scala
+++ src/test/scala/objektwerks/FastparseTest.scala
@@ -3,6 +3,6 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-final class FPTest extends AnyFunSuite with Matchers:
+final class FastparseTest extends AnyFunSuite with Matchers:
   test("todo"):
     1 shouldBe 1
