226c4e41e4d89573ec400bf58a14a85dfe7219fc
objektwerks <objektwerks@runbox.com>
2025-04-15 11:58:57 -0400

    enhanced parser test

--- src/test/scala/objektwerks/ParserTest.scala
+++ src/test/scala/objektwerks/ParserTest.scala
@@ -9,3 +9,4 @@ final class ParserTest extends AnyFunSuite with Matchers:
   test("parser"):    
     parseAndCalc("1+1") shouldBe "2"
     parseAndCalc("(1+1*2)+3*4") shouldBe "15"
+    parseAndCalc("((1+1*2)+(3*4*5))/3") shouldBe "21"
