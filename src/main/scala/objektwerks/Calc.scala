7e5b7a7515711c03aa42e5e4d41331a978bb60a4
objektwerks <objektwerks@runbox.com>
2025-04-15 11:53:09 -0400

    edited calc

--- src/main/scala/objektwerks/Calc.scala
+++ src/main/scala/objektwerks/Calc.scala
@@ -1,5 +1,3 @@
 package objektwerks
 
-@main def calc(args: String*): Unit =
-  val expression = args.mkString
-  println( Parser.calc(expression) )
+@main def calc(args: String*): Unit = println( Parser.calc( args.mkString ) )
