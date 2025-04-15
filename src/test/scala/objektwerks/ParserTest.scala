cf5784356e574b482bc3e50b1d4909cc67272c1f
objektwerks <objektwerks@runbox.com>
2025-04-15 06:45:36 -0400

    enhanced parser test

--- src/test/scala/objektwerks/ParserTest.scala
+++ src/test/scala/objektwerks/ParserTest.scala
@@ -1,8 +1,20 @@
 package objektwerks
 
+import fastparse.* 
+import NoWhitespace.*
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 final class ParserTest extends AnyFunSuite with Matchers:
-  test("todo"):
-    1 shouldBe 1
+  test("parser"):
+    val Parsed.Success(2, _) = parse("1+1", expr(_))
+    val Parsed.Success(15, _) = parse("(1+1*2)+3*4", expr(_))
+    val Parsed.Success(21, _) = parse("((1+1*2)+(3*4*5))/3", expr(_))
+    val Parsed.Failure(expected, failIndex, extra) = parse("1+1*", expr(_))
+    val longAggMsg = extra.trace().longAggregateMsg
+    assert(
+      failIndex == 4,
+      longAggMsg ==
+      """Expected expr:1:1 / addSub:1:1 / divMul:1:3 / factor:1:5 / (number | parens):1:5, found """""
+    )
