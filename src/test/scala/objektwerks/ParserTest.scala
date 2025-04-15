af2bbc0bf27e835c82f3b311a096e84ad550a951
objektwerks <objektwerks@runbox.com>
2025-04-15 16:15:16 -0400

    edited parser tet

--- src/test/scala/objektwerks/ParserTest.scala
+++ src/test/scala/objektwerks/ParserTest.scala
@@ -9,7 +9,7 @@ final class ParserTest extends AnyFunSuite with Matchers:
   test("success"):    
     parseAndCalc("1+1") shouldBe "2"
     parseAndCalc("2-1") shouldBe "1"
-    parseAndCalc("2*2") shouldBe "4" // must use double quotes on macOs
+    parseAndCalc("2*2") shouldBe "4" //fails on macOs
     parseAndCalc("4/2") shouldBe "2"
     parseAndCalc("(1+1*2)+3*4") shouldBe "15" //  must use double quotes on macOs
     parseAndCalc("((1+1*2)+(3*4*5))/3") shouldBe "21" //  must use double quotes on macOs
