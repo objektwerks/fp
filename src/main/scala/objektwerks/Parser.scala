679c5acce5b9c36e31ef6efdaaa1226cf05a8e65
objektwerks <objektwerks@runbox.com>
2025-04-15 06:34:09 -0400

    enhanced parser

--- src/main/scala/objektwerks/Parser.scala
+++ src/main/scala/objektwerks/Parser.scala
@@ -4,10 +4,22 @@ import fastparse.*
 import NoWhitespace.*
 
 class Parser:
+  def eval(tree: (Int, Seq[(String, Int)])) = {
+    val (base, ops) = tree
+    ops.foldLeft(base){ case (left, (op, right)) => op match{
+      case "+" => left + right case "-" => left - right
+      case "*" => left * right case "/" => left / right
+    }}
+  }
+
   def number[$: P]: P[Int] = P( CharIn("0-9").rep(1).!.map(_.toInt) )
-  def parens[$: P]: P[Int] = P( "(" ~/ addSub ~ ")" )
+
+  def parens[$: P]: P[Int] = P( "(" ~/ addSubtract ~ ")" )
+
   def factor[$: P]: P[Int] = P( number | parens )
 
-  def divMul[$: P]: P[Int] = P( factor ~ (CharIn("*/").! ~/ factor).rep ).map(eval)
-  def addSub[$: P]: P[Int] = P( divMul ~ (CharIn("+\\-").! ~/ divMul).rep ).map(eval)
-  def expr[$: P]: P[Int]   = P( addSub ~ End )
+  def divideMultiply[$: P]: P[Int] = P( factor ~ (CharIn("*/").! ~/ factor).rep ).map(eval)
+
+  def addSubtract[$: P]: P[Int] = P( divideMultiply ~ (CharIn("+\\-").! ~/ divideMultiply).rep ).map(eval)
+
+  def expr[$: P]: P[Int]   = P( addSubtract ~ End )
